586a1133d9a2dc3a24e45e5ad1e369c2f1c37855
Asierjunquera1 <ajunquerabartolome@gmail.com>
2022-12-18 14:47:43 +0100

    g

--- estrella.py
+++ estrella.py
@@ -1,1 +1,21 @@
 from turtle import *
+def dibujar_estrella(n):
+    a=n
+    while a>0:
+        a-=2
+    if a==-1:
+        begin_fill()
+        while True:
+            forward(100)
+            left(180+(180/n))
+            if abs(pos()) < 1:
+                break
+        done()
+    elif a==0:
+        begin_fill
+        while True:
+            forward(100)
+            left((180+(180/(n/2))))
+            if abs(pos()) < 1:
+                break
+        done()
